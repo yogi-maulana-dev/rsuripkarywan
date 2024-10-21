e0af0f80816259cd8d684e39e09460005e9d4ddb
yogi-maulana-dev <yogi.maulana.2021211032@mail.darmajaya.ac.id>
2024-10-21 16:24:32 +0700

    karyawan_yogi_maulana

--- hr.sql
+++ hr.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Oct 21, 2024 at 06:28 AM
+-- Generation Time: Oct 21, 2024 at 09:23 AM
 -- Server version: 8.3.0
 -- PHP Version: 8.2.18
 
@@ -55,7 +55,14 @@ CREATE TABLE IF NOT EXISTS `karyawan` (
   `created_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `karyawan`
+--
+
+INSERT INTO `karyawan` (`id`, `nama`, `tgl_lahir`, `gaji`, `created_at`, `deleted_at`) VALUES
+(10, 'yota', '1844-01-17', 80000, NULL, NULL);
 
 --
 -- Triggers `karyawan`
@@ -64,7 +71,7 @@ DROP TRIGGER IF EXISTS `trg_karyawan_delete`;
 DELIMITER $$
 CREATE TRIGGER `trg_karyawan_delete` AFTER DELETE ON `karyawan` FOR EACH ROW BEGIN
   INSERT INTO tlog (tanggal, jam, keterangan, id_karyawan) 
-  VALUES (OLD.tgl_lahir, NOW(), 3, OLD.id); 
+  VALUES (OLD.tgl_lahir, NOW(), 3, OLD.id); -- Keterangan 3: Delete
 END
 $$
 DELIMITER ;
@@ -72,7 +79,7 @@ DROP TRIGGER IF EXISTS `trg_karyawan_insert`;
 DELIMITER $$
 CREATE TRIGGER `trg_karyawan_insert` AFTER INSERT ON `karyawan` FOR EACH ROW BEGIN
   INSERT INTO tlog (tanggal, jam, keterangan, id_karyawan) 
-  VALUES (NEW.tgl_lahir, NOW(), 1, NEW.id); 
+  VALUES (NEW.tgl_lahir, NOW(), 1, NEW.id); -- Keterangan 1: Insert
 END
 $$
 DELIMITER ;
@@ -80,7 +87,7 @@ DROP TRIGGER IF EXISTS `trg_karyawan_update`;
 DELIMITER $$
 CREATE TRIGGER `trg_karyawan_update` AFTER UPDATE ON `karyawan` FOR EACH ROW BEGIN
   INSERT INTO tlog (tanggal, jam, keterangan, id_karyawan) 
-  VALUES (NEW.tgl_lahir, NOW(), 2, NEW.id); 
+  VALUES (NEW.tgl_lahir, NOW(), 2, NEW.id); -- Keterangan 2: Update
 END
 $$
 DELIMITER ;
@@ -160,13 +167,17 @@ CREATE TABLE IF NOT EXISTS `tlog` (
   `keterangan` int NOT NULL,
   `id_karyawan` bigint NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `tlog`
 --
 
 INSERT INTO `tlog` (`id`, `tanggal`, `jam`, `keterangan`, `id_karyawan`) VALUES
+(26, '1995-01-17', '2024-10-21 09:02:32', 3, 9),
+(25, '1995-01-17', '2024-10-21 09:01:39', 2, 9),
+(24, '1844-01-17', '2024-10-21 08:38:15', 1, 10),
+(23, '1844-01-17', '2024-10-21 08:34:23', 1, 9),
 (22, '1955-01-17', '2024-10-21 06:27:11', 3, 8),
 (21, '1955-01-17', '2024-10-21 06:27:00', 1, 8);
 
@@ -188,7 +199,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'yogi', 'yogi11@gmail.com', NULL, '$2y$12$wNP.lxHKkPL92TwHS7EhEOTg9EYMiv76swLPiWsayaPN1a6ElDMke', NULL, '2024-10-21 00:36:29', '2024-10-21 00:36:29');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
